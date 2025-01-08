82e71bbef9ddbd887ef6944bf8ef7265e5ecd47c
lucasdinizselect <155669369+lucasdinizselect@users.noreply.github.com>
2025-01-08 14:32:25 -0300

    subida do ambiente

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,15 @@
-variable "project_name" {}
-variable "region" {}
-variable "capacity_providers" {}
+variable "project_name" {
+  description = "O nome do projeto, usado para nomear recursos no escopo deste Terraform."
+  type        = string
+}
+variable "region" {
+  description = "A região da AWS onde os recursos serão criados."
+  type        = string
+}
+variable "capacity_providers" {
+  type        = list(any)
+  description = "A lista dos capacity providers que serão permitidos no cluster fargate"
+  default = [
+    "FARGATE", "FARGATE_SPOT"
+  ]
+}
