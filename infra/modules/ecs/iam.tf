d695b991aa4de6b4a5c2956cdb3359f4bef6445a
lucasdinizselect <155669369+lucasdinizselect@users.noreply.github.com>
2025-01-27 20:25:30 -0300

    correcao da politica

--- infra/modules/ecs/iam.tf
+++ infra/modules/ecs/iam.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role" "service_execution_role" {
-
   name = format("%s-service-role", var.service_name)
 
-  assume_role_policy = jsondecode({
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "",
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "ecs-tasks.amazonaws.com"
-                },
-                "Action": "sts:AssumeRole"
-            },
-        ]
-    }) 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+
 }
 
 resource "aws_iam_role_policy" "service_execution_role" {
