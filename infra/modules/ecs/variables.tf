8bc11bd804073c8f422455b7eecc01276b7565f6
lucasdinizselect <155669369+lucasdinizselect@users.noreply.github.com>
2025-01-27 20:57:15 -0300

    correcao

--- infra/modules/ecs/variables.tf
+++ infra/modules/ecs/variables.tf
@@ -4,7 +4,3 @@ variable "service_name" {
   description = "Nome do serviço a ser utilizado no ECS ou identificador similar."
 }
 
-variable "task_role" {
-  type        = string
-  description = "Nome da task role para o serviço do ECS"
-}
