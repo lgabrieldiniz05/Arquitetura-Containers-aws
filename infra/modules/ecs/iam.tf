90b0385ff792e0d523969669d422105e6a7ec72f
lucasdinizselect <155669369+lucasdinizselect@users.noreply.github.com>
2025-01-27 20:31:47 -0300

    correcao da politica

--- infra/modules/ecs/iam.tf
+++ infra/modules/ecs/iam.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role_policy" "service_execution_role" {
     name = format("%s-service-policy", var.service_name)
     role = aws_iam_role.service_execution_role.id
 
-    policy = jsondecode({
+    policy = {
     "Version": "2012-10-17"
     "Statement": [
         {
@@ -40,7 +40,7 @@ resource "aws_iam_role_policy" "service_execution_role" {
             "Resource": "*"
         },
     ]
-})
+}
   
 }
 
