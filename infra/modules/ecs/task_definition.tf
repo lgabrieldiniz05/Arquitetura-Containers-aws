fb33b68bf5156ef0299faa24ee8071eb4fc17f59
lucasdinizselect <155669369+lucasdinizselect@users.noreply.github.com>
2025-01-28 09:15:43 -0300

    ajustes

--- infra/modules/ecs/task_definition.tf
+++ infra/modules/ecs/task_definition.tf
@@ -6,33 +6,29 @@ resource "aws_ecs_task_definition" "main" {
     memory = var.service_memory
 
 
-    container_definitions = jsondecode([
-        {
-        name = var.service_name 
+    container_definitions = jsonencode([
+      {
+        name  = var.service_name
         image = format("%s:latest", aws_ecr_repository.main.repository_url)
-        cpu = var.service_cpu
+        cpu   = var.service_cpu
         memory = var.service_memory
-        
         essential = true
-
         portMappings = [
-        {
-          containerPort = var.service_port
-          hostPort      = var.service_port
-          protocol      = "tcp"
+          {
+            containerPort = var.service_port
+            hostPort      = var.service_port
+            protocol      = "tcp"
+          }
+        ]
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.main.id
+            awslogs-region        = var.region
+            awslogs-stream-prefix = var.service_name
+          }
         }
-      ]
-
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.main.id
-          awslogs-region        = var.region
-          awslogs-stream-prefix = var.service_name
-        }
-
       }
-    }
     ])
     
 }
